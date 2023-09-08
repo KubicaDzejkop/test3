c4ddb1f11cc201fd1400edf2b3be4e3e9f173ec0
KubicaDzejkop <kubix1wree@gmail.com>
2023-09-08 09:34:44 +0200

    silnia

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        // Press Alt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            System.out.println("i = " + i);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        int nr = skaner.nextInt();
+        int silnia = 1;
+        for(int i=1; i<=nr; i++){
+            silnia *= i;
 
-            System.out.println("i = " + i);
+        }
+        System.out.println("Silnia wynosi " +silnia);
         }
     }
-}
